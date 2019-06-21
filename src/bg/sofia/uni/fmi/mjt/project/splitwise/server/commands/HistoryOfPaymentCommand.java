615d4f5c4a5171c9f7611fb45a15778f7926b51c
Todor <todorangelov_97@abv.bg>
2019-06-22 01:10:05 +0300

    Refactor HistoryOfPayment for readability

--- src/bg/sofia/uni/fmi/mjt/project/splitwise/server/commands/HistoryOfPaymentCommand.java
+++ src/bg/sofia/uni/fmi/mjt/project/splitwise/server/commands/HistoryOfPaymentCommand.java
@@ -2,15 +2,21 @@ package bg.sofia.uni.fmi.mjt.project.splitwise.server.commands;
 
 import bg.sofia.uni.fmi.mjt.project.splitwise.server.Domain;
 import bg.sofia.uni.fmi.mjt.project.splitwise.server.Server;
-import bg.sofia.uni.fmi.mjt.project.splitwise.server.ServerOld;
+import bg.sofia.uni.fmi.mjt.project.splitwise.utilitis.Commands;
 
 import java.io.*;
 
 public class HistoryOfPaymentCommand extends ActionCommand {
 
+    private Server server;
+    private PrintWriter writer;
+    private String username;
 
     public HistoryOfPaymentCommand(Domain domain, PrintWriter writer) {
         super(domain, writer);
+        server = getDomain().getServer();
+        writer = getWriter();
+        username = getDomain().getUsername();
     }
 
     @Override
@@ -23,25 +29,18 @@ public class HistoryOfPaymentCommand extends ActionCommand {
 
     @Override
     protected boolean isMatched(String command) {
-        if ("history-of-payment".equals(command)) {
-            return true;
-        }
-        return false;
+        return Commands.HISTORY_OF_PAYMENT.equals(command);
     }
 
     private void getHistoryOfPayment() {
 
-        Server server = getDomain().getServer();
-        String username = getDomain().getUsername();
         File file = server.getFile(username);
-
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             while (true) {
                 final String line = reader.readLine();
                 if (line == null) {
                     break;
                 }
-                PrintWriter writer = getWriter();
                 writer.println(line);
             }
         } catch (IOException e) {
