2eb8edca3f1a19cab640eefe4ee103978685ef56
Todor <todorangelov_97@abv.bg>
2019-06-22 00:57:57 +0300

    Refactor AddFriendCommand for readabilit

--- src/bg/sofia/uni/fmi/mjt/project/splitwise/server/commands/AddFriendCommand.java
+++ src/bg/sofia/uni/fmi/mjt/project/splitwise/server/commands/AddFriendCommand.java
@@ -2,14 +2,22 @@ package bg.sofia.uni.fmi.mjt.project.splitwise.server.commands;
 
 import bg.sofia.uni.fmi.mjt.project.splitwise.server.Domain;
 import bg.sofia.uni.fmi.mjt.project.splitwise.server.Server;
+import bg.sofia.uni.fmi.mjt.project.splitwise.utilitis.Commands;
 
 import java.io.PrintWriter;
 
 public class AddFriendCommand extends ActionCommand {
 
+    private Server server;
+    private PrintWriter writer;
+    private String username;
+
 
     public AddFriendCommand(Domain domain, PrintWriter writer) {
         super(domain, writer);
+        server = getDomain().getServer();
+        writer = getWriter();
+        username = getDomain().getUsername();
     }
 
     @Override
@@ -23,10 +31,7 @@ public class AddFriendCommand extends ActionCommand {
 
     @Override
     protected boolean isMatched(String command) {
-        if ("add-friend".equals(command)) {
-            return true;
-        }
-        return false;
+        return Commands.ADD.getCommand().equals(command);
     }
 
     private void addFriend(String[] tokens) {
@@ -34,18 +39,14 @@ public class AddFriendCommand extends ActionCommand {
         if (tokens.length == 2) {
             add(tokens);
         } else {
-            PrintWriter writer = getWriter();
             writer.println(ERROR_MESSAGE);
         }
     }
 
     private void add(String[] tokens) {
 
-        Server server = getDomain().getServer();
         final int FRIEND_INDEX = 1;
         String friend = tokens[FRIEND_INDEX];
-        PrintWriter writer = getWriter();
-        String username = getDomain().getUsername();
         if (!server.isUsernameContained(friend)) {
             writer.println(String.format("User with name: %s does not exists.", friend));
         } else if (server.isUserInFriends(username, friend)) {
@@ -53,14 +54,13 @@ public class AddFriendCommand extends ActionCommand {
         } else if (username.equals(friend)) {
             writer.println("You can not add yourself as a friend.");
         } else {
-            addToFriendList(username, friend);
+            addToFriendList(friend);
             String message = String.format("%s added you as friend. %n", server.getProfileNames(username));
             sendFriendNotification(friend, message);
         }
     }
 
-    private void addToFriendList(String username, String friend) {
-        Server server = getDomain().getServer();
+    private void addToFriendList(String friend) {
         server.addFriend(username, friend);
         server.addFriend(friend, username);
 
@@ -69,7 +69,6 @@ public class AddFriendCommand extends ActionCommand {
     }
 
     private void sendFriendNotification(String friend, String message) {
-        Server server = getDomain().getServer();
         server.sendFriendNotification(friend, message);
     }
 }
